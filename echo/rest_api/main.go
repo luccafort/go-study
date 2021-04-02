ec6222cfc674d2af8230103e7f7f20c8635ac447
luccafort <konishi.tatsuro@moneyforward.co.jp>
2021-04-02 20:11:24 +0900

    Echoを使ったGETとPOSTの仮APIを実装
    - `GET /users/:id` APIを実装
    - `POST /users/` APIを実装

---
+++ echo/rest_api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(context echo.Context) error {
+		return context.String(http.StatusOK, "Hello, Gopher World!")
+	})
+
+	// /users/
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUserByID)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func createUser(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	fmt.Printf("name: %v, email: %v", name, email)
+	return c.String(http.StatusOK, fmt.Sprintf("name: %v, email: %v", name, email))
+}
+
+func getUserByID(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintln(id))
+}
