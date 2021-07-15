3486e37993d7f5b3d45995b96593daf49a616bb5
luccafort <konishi.tatsuro@moneyforward.co.jp>
2021-07-15 19:19:54 +0900

    リスト3.4のサンプルコードを実装

--- web_application_in_go/chapter_3/3_1.go
+++ web_application_in_go/chapter_3/3_1.go
@@ -8,11 +8,12 @@ import (
 func main() {
 	fmt.Println("wake up http server.")
 
-	// リスト3.3設定を加えたWebサーバ
+	// リスト3.3 設定を加えたWebサーバ
 	server := http.Server{
 		Addr:    "120.0.0.1:8080",
 		Handler: nil,
 	}
 
-	server.ListenAndServe()
+	// リスト3.4 HTTPSによるサービス
+	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
